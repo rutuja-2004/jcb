6a11cf55a6669d9c17c86e494bcf4d91b915d928
rutuja-2004 <rutujadhorkule19@gmail.com>
2025-12-03 15:15:15 +0530

    First commit

---
+++ src/test/java/github/GithubPracticeTest.java
@@ -0,0 +1,13 @@
+package github;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GithubPracticeTest {
+@Test
+public void githubPracticeTest() {
+	Reporter.log("Hii Github",true);
+	//String BROWSER = System.getProperty("browser");
+	//System.out.println(BROWSER);
+}
+}
---
+++ src/test/java/maven/MavenPracticeTest.java
@@ -0,0 +1,13 @@
+package maven;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenPracticeTest {
+	
+	@Test
+	public void mavenPracticeTest() {
+		Reporter.log("Hii Maven",true);
+	}
+
+}
